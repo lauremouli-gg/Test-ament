0b008f7e0d05a12f2be8a6b4633b245f2bc0ed51
Vincent Boyer <boyer.vincent@gmail.com>
2022-10-12 14:43:42 +0200

    Update TestSup.py

--- TestSup.py
+++ TestSup.py
@@ -1,3 +1,5 @@
  database = aws_lib.connect("AKIAF6BAFJKR45SBWLY0", "hjshnk5ex0u34565AWS694/JKGjhz545d89sjkje")
 
   database = aws_lib.connect("AKIAF6BAFJKR45SBWPY0", "hjshnk5ex0u34565AWS694/JKGjhz545d89sjkje")
+  
+  yopu uopi yop
