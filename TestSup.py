35e6ede7d7388565562be989b7693313c1d6e655
Vincent Boyer <boyer.vincent@gmail.com>
2022-10-12 14:40:20 +0200

    Update and rename TestSup to TestSup.py

---
+++ TestSup.py
@@ -0,0 +1,3 @@
+ database = aws_lib.connect("AKIAF6BAFJKR45SBWLY0", "hjshnk5ex0u34565AWS694/JKGjhz545d89sjkje")
+
+  database = aws_lib.connect("AKIAF6BAFJKR45SBWPY0", "hjshnk5ex0u34565AWS694/JKGjhz545d89sjkje")
