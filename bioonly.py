61f5a5652178b96c0a1e6b2c61580c350cb479ad
Vincent Boyer <boyer.vincent@gmail.com>
2023-07-19 20:15:08 +0200

    Update bioonly.py

--- bioonly.py
+++ bioonly.py
@@ -1,1 +1,1 @@
-database = aws_lib.connect("AKIAG6BAFJKR05DBWPY0", "hjshkk5ex0u34505BWS194/JKGjhz545d89sjkje")
+database = aws_lib.connect("AKIAG6BAFJKR05FBWPY0", "hjshkk5ex0u34509BWS194/JKGjhz545d89sjkje")
