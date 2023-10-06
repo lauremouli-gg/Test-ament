9fe58e31d4675cffe66bf6e9e0d16acab732b04e
lauremouli-gg <147169802+lauremouli-gg@users.noreply.github.com>
2023-10-06 16:29:58 +0200

    Test

--- wooo.py
+++ wooo.py
@@ -1,8 +1,6 @@
- database = aws_lib.connect("AKIAG6BAFJKR45SBWLY0", "hjshnk5ex0u39965AWS694/JKGjhz545d89sjkje")
+ database = aws_lib.connect("AKIAR6BAFJKR45SBWLY0", "hjshnk5ex0u39965AWS694/JKGjhz545d89sjkje")
 
 sdfsdf
 sdfs
 dfs
 dfsdf
-
- database = aws_lib.connect("AKIAG6BAFBKR45SBWLY0", "hjshnk5ex0u39965AWQ694/JKGjhz545d89sjkje")
