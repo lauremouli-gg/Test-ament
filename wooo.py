9e925216a069ab81ec0ad4be2f571a7f74a01059
lauremouli-gg <147169802+lauremouli-gg@users.noreply.github.com>
2023-10-06 16:51:04 +0200

    Update wooo.py

--- wooo.py
+++ wooo.py
@@ -1,4 +1,4 @@
- database = aws_lib.connect("AKIAR6BAFJKR45SBWLY0", "hjshnk5ex0u39965AWS694/JKGjhz545d89sjkje")
+ database = aws_lib.connect("AKIAT6BAFJKR45SBWLY0", "hjshnk5ex0u39965AWS694/JKGjhz545d89sjkje")
 
 sdfsdf
 sdfs
