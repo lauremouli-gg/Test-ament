72fe84bb053b38392c13c024d1a738d9eaeb6dee
Laure Mouli <laure.mouli@gitguardian.com>
2024-10-16 15:24:50 +0200

    test

--- last_test.py
+++ last_test.py
@@ -1,1 +1,1 @@
- database = aws_lib.connect("AKIAF6CAFJKR45SBWLY0", "hjshnk5ex0u39565AWS094/JKGjhz545d89sjkje")
+ database = aws_lib.connect("AKIAF6CAFJKR45SBWLY9", "hjshnk5ex0u39565AWS094/JKGjhz545d89sjkje")
